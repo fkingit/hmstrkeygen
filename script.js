438d8d739b915a332e4cfa8c1739ba6c5e4b9f32
fkingit <good-passion-tiny@duck.com>
2024-09-22 00:08:09 +0530

    update script.js

--- script.js
+++ script.js
@@ -319,7 +319,7 @@ document.getElementById('startBtn').addEventListener('click', async () => {
 });
 
 document.getElementById('creatorChannelBtn').addEventListener('click', () => {
-    window.location.href = 'https://t.me/pdosi_project';
+    window.location.href = 'https://t.me/+1Fm-8cnSsDRiYTYx';
 });
 
 function generateClientId() {
